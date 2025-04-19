94e8c68a24b6a8491f57daa832f56436e0216f2d
Janpierry Santana <janpierry2004@gmail.com>
2025-04-19 08:06:34 -0300

    adicionado watsapp

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -18,3 +18,18 @@ function toggleFolder() {
     const icons = document.querySelector('.app-icons');
     icons.classList.toggle('show');
 }
+
+//whatsapp
+document.getElementById('whatsapp-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const nome = document.getElementById('nome').value.trim();
+    const mensagem = document.getElementById('mensagem').value.trim();
+
+    if (nome && mensagem) {
+        const numero = '11988806169';
+        const texto = `Olá, me chamo ${nome}. ${mensagem}`;
+        const url = `https://wa.me/${numero}?text=${encodeURIComponent(texto)}`;
+        window.open(url, '_blank');
+    }
+});
