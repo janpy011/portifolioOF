556af63e8a989be4a2980e92d92e9835265c844e
Janpierry Santana <janpierry2004@gmail.com>
2025-04-19 06:42:49 -0300

    habilidades adicionada

---
+++ assets/js/sobreMim.js
@@ -0,0 +1,17 @@
+const animarOnScroll = () => {
+    const elementos = document.querySelectorAll('[data-animar]');
+
+    elementos.forEach(el => {
+        const posicao = el.getBoundingClientRect().top;
+        const alturaTela = window.innerHeight * 0.85;
+
+        if (posicao < alturaTela) {
+            const delay = el.dataset.delay || '0s'; // default 0s
+            el.style.setProperty('--delay', delay);
+            el.classList.add('ativo');
+        }
+    });
+};
+
+window.addEventListener('scroll', animarOnScroll);
+window.addEventListener('load', animarOnScroll);
