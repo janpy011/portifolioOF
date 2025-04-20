1648f7e021405cdbf2c3a01f0c703a86dbd951e9
Janpierry Santana <janpierry2004@gmail.com>
2025-04-20 15:29:10 -0300

    adicionado otimizaçoes

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,3 +50,70 @@ const sr = ScrollReveal({
 
 sr.reveal('.section__data', { origin: 'left', distance: '50px' });
 sr.reveal('.section__img', { origin: 'right', distance: '50px', delay: 200 });
+
+/* ===== TEXTO DIGITANDO NO LOGO COM CURSOR PISCANDO ===== */
+const textosLogo = ['Janpierry', 'Sobre Mim ←'];
+let logoIndex = 0;
+const logo = document.querySelector('.nav__logo');
+
+logo.style.position = 'relative';
+
+const style = document.createElement('style');
+style.innerHTML = `
+.nav__logo::after {
+    content: '|';
+    animation: blink 1s step-end infinite;
+    position: absolute;
+    right: -10px;
+    top: 0;
+}
+@keyframes blink {
+    50% { opacity: 0; }
+}
+`;
+document.head.appendChild(style);
+
+function escreverTexto(texto, callback) {
+    let i = 0;
+    logo.textContent = '';
+
+    function digitar() {
+        if (i < texto.length) {
+            logo.textContent += texto.charAt(i);
+            i++;
+            setTimeout(digitar, 100);
+        } else if (callback) {
+            setTimeout(callback, 1500);
+        }
+    }
+
+    digitar();
+}
+
+function apagarTexto(callback) {
+    let texto = logo.textContent;
+    let i = texto.length;
+
+    function apagar() {
+        if (i > 0) {
+            logo.textContent = texto.substring(0, i - 1);
+            i--;
+            setTimeout(apagar, 50);
+        } else if (callback) {
+            callback();
+        }
+    }
+
+    apagar();
+}
+
+function animarLogo() {
+    escreverTexto(textosLogo[logoIndex], () => {
+        apagarTexto(() => {
+            logoIndex = (logoIndex + 1) % textosLogo.length;
+            animarLogo();
+        });
+    });
+}
+
+animarLogo();
